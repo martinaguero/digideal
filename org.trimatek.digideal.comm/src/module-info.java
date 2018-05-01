55709551214708d3784b2a6022758f30695283b6
Martín <aguero.martin@gmail.com>
2018-05-01 13:34:03 -0300

    vertx post

--- org.trimatek.digideal.comm/src/module-info.java
+++ org.trimatek.digideal.comm/src/module-info.java
@@ -1,6 +1,7 @@
 module org.trimatek.digideal.comm {
 	exports org.trimatek.digideal.comm.mail;
 
+	requires org.trimatek.digideal.model;
 	requires google.api.client;
 	requires google.api.services.gmail.v1.rev82;
 	requires google.http.client;
@@ -14,4 +15,6 @@ module org.trimatek.digideal.comm {
 	requires java.logging;
 	requires vertx.core;
 	requires vertx.web;
+	requires gson;
+	requires java.sql;
 }
--- org.trimatek.digideal.comm/src/org/trimatek/digideal/comm/rest/Server.java
+++ org.trimatek.digideal.comm/src/org/trimatek/digideal/comm/rest/Server.java
@@ -1,12 +1,16 @@
 package org.trimatek.digideal.comm.rest;
 
+import org.trimatek.digideal.model.Draft;
+
+import com.google.gson.Gson;
+
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
-import io.vertx.core.http.HttpServer;
-import io.vertx.core.http.HttpServerOptions;
-import io.vertx.core.net.JksOptions;
+import io.vertx.core.http.HttpServerResponse;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
 
 public class Server extends AbstractVerticle {
 
@@ -23,13 +27,45 @@ public class Server extends AbstractVerticle {
   public void start(Future<Void> startFuture) throws Exception {
 
     Router router = Router.router(vertx);
+    
+    router.route("/").handler(routingContext -> {
+        HttpServerResponse response = routingContext.response();
+        response
+            .putHeader("content-type", "text/html")
+            .end("<h1>Hola, soy DigiDeal</h1>");
+  });
+    
+    router.route("/api/drafts*").handler(BodyHandler.create());
+    router.post("/api/drafts").handler(this::addOne);
+    
+    
+    vertx
+        .createHttpServer()
+        .requestHandler(router::accept)
+        .listen(
+            config().getInteger("http.port", 8080),
+            result -> {
+              if (result.succeeded()) {
+            	  startFuture.complete();
+              } else {
+            	  startFuture.fail(result.cause());
+              }
+            }
+        );
 
-    router.route().handler(ctx -> {
-      ctx.request().response().end("Hello World");
-    });
-
-    vertx.createHttpServer()
-      .requestHandler(router::accept)
-      .listen(8080, ar -> startFuture.handle(ar.mapEmpty()));
   }
+ 
+  private void addOne(RoutingContext routingContext) {
+
+	    final Draft draft = new Gson().fromJson(routingContext.getBodyAsString(),Draft.class);
+
+	    System.out.println(draft.getText());
+
+	    routingContext.response()
+	        .setStatusCode(201)
+	        .putHeader("content-type", "application/json; charset=utf-8")
+	        .end(new Gson().toJson(draft));
+	}
+  
+  
 }
