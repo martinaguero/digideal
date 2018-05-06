70e7323396c58eb9cf3c8dc7811a189246a9bfd0
Martín <aguero.martin@gmail.com>
2018-05-06 20:27:15 -0300

    Compiler coco

--- org.trimatek.digideal.compiler/src/module-info.java
+++ org.trimatek.digideal.compiler/src/module-info.java
@@ -1,4 +1,5 @@
 module org.trimatek.digideal.compiler {
 	exports org.trimatek.digideal.compiler;
 	requires org.trimatek.digideal.model;
+	requires org.trimatek.digideal.bitcoin;
 }
--- org.trimatek.digideal.compiler/src/org/trimatek/digideal/compiler/Compiler.java
+++ org.trimatek.digideal.compiler/src/org/trimatek/digideal/compiler/Compiler.java
@@ -1,10 +1,28 @@
 package org.trimatek.digideal.compiler;
 
+import org.trimatek.digideal.bitcoin.tools.ReadStream;
+
+
+
 public class Compiler {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
 
+		
+		Process proc = Runtime.getRuntime().exec("C:\\Program Files\\Java\\jdk1.8.0_60\\bin\\java -jar d:\\bin\\coco\\coco.jar d:\\Temp\\coco\\quinto.cnt");
+		// Then retreive the process output
+		ReadStream s1 = new ReadStream("stdin", proc.getInputStream());
+		ReadStream s2 = new ReadStream("stderr", proc.getErrorStream());
+		s1.start();
+		s2.start();
+		proc.waitFor();
+
+		String in = s1.getStream();
+		String err = s2.getStream();
+		
+		System.out.println(in);
+		System.out.println(err);
+		
 	}
 
 }
