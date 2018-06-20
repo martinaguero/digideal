cdea043411e4db5f7aeb38b321bd3c326a3cf786
Martín <aguero.martin@gmail.com>
2018-06-20 10:55:31 -0300

    draft

--- org.trimatek.digideal.ui/src/org/trimatek/digideal/ui/utils/SourceBuilder.java
+++ org.trimatek.digideal.ui/src/org/trimatek/digideal/ui/utils/SourceBuilder.java
@@ -7,12 +7,22 @@ public class SourceBuilder {
 
 	static public String getDraft(ContractView view) {
 		StringBuilder sb = new StringBuilder();
-		sb.append(Config.COMMA_SYMBOL + Tools.msg.getString("contract_header") + " ");
+		sb.append(Config.COMMA_SYMBOL + Tools.msg.getString("contract_header") + "<br/>");
 		sb.append(Tools.msg.getString("contract_mr_mrs") + " " + view.getNamePayer());
-		sb.append(Tools.msg.getString("contract_identified_by_payer")+ Config.COMMA_SYMBOL  + " " + view.getNickPayer() + " {"
-				+ view.getAddressPayer() + ", " + view.getEmailPayer() + "}");
-		sb.append(" " + Tools.msg.getString("contract_and") + " " + Tools.msg.getString("contract_mr_mrs") + " "
-				+ view.getNameCollector());
+		sb.append(Tools.msg.getString("contract_identified_by_payer") + Config.COMMA_SYMBOL + " " + view.getNickPayer()
+				+ " {" + view.getAddressPayer() + ", " + view.getEmailPayer() + "} "
+				+ Tools.msg.getString("contract_and") + "<br/>");
+		sb.append(Tools.msg.getString("contract_mr_mrs") + " " + view.getNameCollector());
+		sb.append(Tools.msg.getString("contract_identified_by_collector") + Config.COMMA_SYMBOL + " "
+				+ view.getNickCollector() + " {" + view.getAddressCollector() + ", " + view.getEmailCollector() + "}"
+				+ "<br/>");
+		sb.append(Tools.msg.getString("contract_establishes") + " " + view.getNickPayer() + " "
+				+ Tools.msg.getString("contract_will_pay") + " " + view.getNickCollector() + " ");
+		sb.append(Tools.msg.getString("contract_the_sum") + " " + view.getSelectedCurrency() + " "
+				+ view.getQuantity() + Tools.msg.getString("contract_with") +  " BTC " + view.getBtc() + " <br/>");
+		sb.append(Tools.msg.getString("contract_if") + " " + view.getNickCollector() + " "
+				+ Tools.msg.getString("contract_delivers") + " " + view.getAddress() + " ");
+		sb.append(Tools.msg.getString("contract_this") + " " + view.getItem() + " <br/>");
 		return sb.toString();
 	}
 
