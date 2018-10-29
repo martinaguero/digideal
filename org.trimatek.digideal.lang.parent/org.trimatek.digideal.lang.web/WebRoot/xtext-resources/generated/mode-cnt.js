75f20e9549dbe619a94aa5923fa0eda98177c538
Martín <aguero.martin@gmail.com>
2018-10-28 21:03:27 -0300

    tBTC

--- org.trimatek.digideal.lang.parent/org.trimatek.digideal.lang.web/WebRoot/xtext-resources/generated/mode-cnt.js
+++ org.trimatek.digideal.lang.parent/org.trimatek.digideal.lang.web/WebRoot/xtext-resources/generated/mode-cnt.js
@@ -1,5 +1,6 @@
 define(["ace/lib/oop", "ace/mode/text", "ace/mode/text_highlight_rules"], function(oop, mText, mTextHighlightRules) {
 	var HighlightRules = function() {
+		var keywords = "DigiDeal";
 		this.$rules = {
 			"start": [
 				{token: "comment", regex: "\\/\\/.*$"},
@@ -8,7 +9,8 @@ define(["ace/lib/oop", "ace/mode/text", "ace/mode/text_highlight_rules"], functi
 				{token: "string", regex: "['](?:(?:\\\\.)|(?:[^'\\\\]))*?[']"},
 				{token: "constant.numeric", regex: "[+-]?\\d+(?:(?:\\.\\d*)?(?:[eE][+-]?\\d+)?)?\\b"},
 				{token: "lparen", regex: "[{]"},
-				{token: "rparen", regex: "[}]"}
+				{token: "rparen", regex: "[}]"},
+				{token: "keyword", regex: "\\b(?:" + keywords + ")\\b"}
 			],
 			"comment": [
 				{token: "comment", regex: ".*?\\*\\/", next : "start"},
